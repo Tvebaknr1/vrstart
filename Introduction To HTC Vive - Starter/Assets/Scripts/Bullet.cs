3f19ed2c286de80ff1256620e20738ec44b57b0d
DESKTOP-L3B06SV\Admin <emil@indeni.dk>
2017-02-27 16:41:49 +0100

    commitmens are hard

---
+++ Introduction To HTC Vive - Starter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    Vector3 target;
+    public int bulletSpeed;
+    public void setTarget(Vector3 target)
+    {
+        this.target = target;
+        transform.LookAt(target);
+        GameObject.Destroy(this.gameObject, 2);
+    }
+    public void Update()
+    {
+        transform.position += Vector3.forward * Time.deltaTime * bulletSpeed;
+    }
+}
---
+++ Introduction To HTC Vive - Starter/Assets/Scripts/gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour {
+
+
+    public GameObject bullet;
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+
+            GameObject temp= GameObject.Instantiate(bullet,transform.position,transform.localRotation);
+            temp.GetComponent<Bullet>().setTarget();
+        }
+
+    }
+}
