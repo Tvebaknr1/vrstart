0925b7d18f35479804cd9308031c57cda2433b39
DESKTOP-L3B06SV\Admin <emil@indeni.dk>
2017-03-02 12:58:42 +0100

    no message

---
+++ Introduction To HTC Vive - Starter/Assets/player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    public GameObject vrplayer;
+    private void Awake()
+    {
+        var temp = Instantiate(vrplayer);
+        temp.transform.parent = this.transform;
+    }
+}
---
+++ Introduction To HTC Vive - Starter/Assets/testing.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour {
+    public GameObject gmae;
+	// Use this for initialization
+	void Start () {
+        Instantiate(gmae);
+	}
+	
+}
