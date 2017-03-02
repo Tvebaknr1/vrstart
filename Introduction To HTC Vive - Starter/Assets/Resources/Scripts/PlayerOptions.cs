8d72cad792e865c4f6e4f2c0f6b5a69b4efe24ab
Lange-Laptop-Pc\Lange-Laptop <peterleolange@yahoo.dk>
2017-03-02 11:53:04 +0100

    multiplayer

---
+++ Introduction To HTC Vive - Starter/Assets/Resources/Scripts/PlayerOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerOptions : MonoBehaviour {
+    public bool usingVR;
+    public GameObject canvas;
+    public void vr(bool vr)
+    {
+        usingVR = vr;
+        this.GetComponent<NetworkManagerHUD>().enabled = true;
+        GameObject.Destroy(canvas);
+    }
+}
---
+++ Introduction To HTC Vive - Starter/Assets/Resources/Scripts/myNetworkManeger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class myNetworkManeger : NetworkManager
+{
+    int i = 0;
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject player;
+        PlayerOptions Options = gameObject.GetComponent<PlayerOptions>();
+        if (i == 0)
+        {
+            player = Instantiate(Resources.Load("Players/testPlayer"), transform.position, Quaternion.identity) as GameObject;
+            i++;
+        }
+        else 
+        {
+            player = Instantiate(Resources.Load("Players/VRPlayer"), transform.position, Quaternion.identity) as GameObject;
+        }
+            
+
+
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+    //Dictionary<int, int> currentPlayers;
+    //public override GameObject OnLobbyServerCreateLobbyPlayer(NetworkConnection conn, short playerControllerId)
+    //{
+    //    if (!currentPlayers.ContainsKey(conn.connectionId))
+    //        currentPlayers.Add(conn.connectionId, 0);
+
+    //    return base.OnLobbyServerCreateLobbyPlayer(conn, playerControllerId);
+    //}
+
+    //public void SetPlayerTypeLobby(NetworkConnection conn, int _type)
+    //{
+    //    if (currentPlayers.ContainsKey(conn.connectionId))
+    //        currentPlayers[conn.connectionId] = _type;
+    //}
+
+    //public override GameObject OnLobbyServerCreateGamePlayer(NetworkConnection conn, short playerControllerId)
+    //{
+    //    int index = currentPlayers[conn.connectionId];
+
+    //    GameObject _temp = (GameObject)GameObject.Instantiate(spawnPrefabs[index],
+    //        startPositions[conn.connectionId].position,
+    //        Quaternion.identity);
+
+    //    NetworkServer.AddPlayerForConnection(conn, _temp, playerControllerId);
+
+    //    return _temp;
+    //}
+}
